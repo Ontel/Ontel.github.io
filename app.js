ae8d8c54ed65b311d7765561f98db34b39c471be
bfunk88 <brandon@ontel.co>
2014-11-05 23:59:49 -0500

    V1. non-responsive yet.

--- app.js
+++ app.js
@@ -3,46 +3,26 @@
  */
 var ontelsite = angular.module('ontelsite',['ngRoute', 'ngAnimate']);
 
-//ontelsite.config(['$routeProvider', function($routeProvider) {
-//    $routeProvider
-//        .when('/', {
-//            templateUrl: 'home.html',
-//            controller: 'MainSiteController'
-//        })
-//        .when('/scorecard', {
-//            templateUrl: 'uplink.html',
-//            controller: 'UplinkController'
-//        })
-//        .when('/sweepanalytics', {
-//            templateUrl: 'sweepanalytics.html',
-//            controller: 'SweepAnalyticsController'
-//        })
-//        .when('/scorecard', {
-//            templateUrl: 'scorecard.html',
-//            controller: 'ScoreCardController'
-//        });
-//
-//}]);
+ontelsite.config(['$routeProvider', '$locationProvider', function($routeProvider,$locationProvider) {
+    $routeProvider
+        .when('/', {
+            templateUrl: 'home.html',
+            controller: 'MainSiteController'
+        })
+        .when('/scorecard', {
+            templateUrl: 'uplink.html',
+            controller: 'UplinkController'
+        })
+        .when('/sweepanalytics', {
+            templateUrl: 'sweepanalytics.html',
+            controller: 'SweepAnalyticsController'
+        })
+        .when('/scorecard', {
+            templateUrl: 'scorecard.html',
+            controller: 'ScoreCardController'
+        });
 
-//ontelsite.directive('scrollOnClick', function() {
-//    return {
-//        restrict: 'A',
-//        link: function(this) {
-//            event.preventDefault();
-//            if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-//                var target = $(this.hash);
-//                target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-//                var scrollToPosition = $(target).offset().top - headerHeight;
-//                if (target.length) {
-//                    $('html,body').animate({
-//                        scrollTop: scrollToPosition
-//                    }, 1000);
-//                    //return false;
-//                }
-//            }
-//        }
-//    }
-//});
+}]);
 
 ontelsite.controller('MainSiteController', ['$scope','$timeout', function($scope, $timeout) {
     $scope.pageClass = 'page-home';
@@ -65,9 +45,87 @@ ontelsite.controller('MainSiteController', ['$scope','$timeout', function($scope
         ;
     }
 
+    $(function() {
 
+        var slrAnimation = function($el) {
+            $el.removeClass('preanimate');
+            $el.addClass('animated fadeInRight');
+        };
+        var sllAnimation = function($el) {
+            $el.removeClass('preanimate');
+            $el.addClass('animated fadeInLeft');
+        };
 
+        $('.sl-l li').each(function(i, el) {
+            setTimeout(function() {sllAnimation($(el))}, i++ * 200);
+        });
 
+        $('.sl-r li').each(function(i, el) {
+            setTimeout(function() {slrAnimation($(el))}, i++ * 200);
+        });
+
+        setTimeout(function() {
+            $('.servicelist .servicedesc').addClass('showservicedesc');
+//    $('.servicelist .servicedesc').addClass('animated rotateInUpLeft');
+        }, 1800);
+        setTimeout(function() {
+            $('.servicelist .servicedesc span').addClass('animated fadeIn');
+            $('.servicelist .servicedesc span').removeClass('preanimate');
+        }, 2200);
+
+        $('#software').waypoint(function() {
+            $('.navactive').removeClass('navactive');
+            $('.homeli').addClass('navactive');
+        },{ offset: '100%' });
+        $('#software').waypoint(function() {
+            $('.navactive').removeClass('navactive');
+            $('.softwareli').addClass('navactive');
+        },{ offset: '25%' });
+        $('#services').waypoint(function() {
+            $('.navactive').removeClass('navactive');
+            $('.servicesli').addClass('navactive');
+        },{ offset: '25%' });
+        $('#about').waypoint(function() {
+            $('.navactive').removeClass('navactive');
+            $('.aboutli').addClass('navactive');
+        },{ offset: '25%' });
+        $('#contact').waypoint(function() {
+            $('.navactive').removeClass('navactive');
+            $('.contactli').addClass('navactive');
+        },{ offset: '50%' });
+
+        var headerHeight = $("#menu").height();
+        $(".homeli").click(function() {
+            $('html, body').animate({
+                scrollTop: $("#landing").offset().top
+            }, 1000);
+        });
+        $(".softwareli").click(function() {
+            $('html, body').animate({
+                scrollTop: $("#software").offset().top - headerHeight
+            }, 1000);
+        });
+        $(".softwaredown").click(function() {
+            $('html, body').animate({
+                scrollTop: $("#software").offset().top - headerHeight
+            }, 1000);
+        });
+        $(".servicesli").click(function() {
+            $('html, body').animate({
+                scrollTop: $("#services").offset().top - headerHeight
+            }, 1000);
+        });
+        $(".aboutli").click(function() {
+            $('html, body').animate({
+                scrollTop: $("#about").offset().top - headerHeight
+            }, 1000);
+        });
+        $(".contactli").click(function() {
+            $('html, body').animate({
+                scrollTop: $("#contact").offset().top - headerHeight
+            }, 1000);
+        });
+    });
 
 }]);
 
